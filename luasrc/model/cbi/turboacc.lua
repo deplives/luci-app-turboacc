5d29668b1a826aaecbc68488a4d2fc43180cce1c
Bobby Zhang <zhang.deplives@hotmail.com>
2021-11-08 13:47:24 +0800

    reformat

--- luasrc/model/cbi/turboacc.lua
+++ luasrc/model/cbi/turboacc.lua
@@ -1,7 +1,7 @@
 local kernel_version = luci.sys.exec("echo -n $(uname -r)")
 
 m = Map("turboacc")
-m.title	= translate("Turbo ACC Acceleration Settings")
+m.title = translate("Turbo ACC Acceleration Settings")
 m:append(Template("turboacc/turboacc_status"))
 
 s = m:section(TypedSection, "turboacc", "")
@@ -9,37 +9,37 @@ s.addremove = false
 s.anonymous = true
 
 if nixio.fs.access("/lib/modules/" .. kernel_version .. "/xt_FLOWOFFLOAD.ko") then
-sw_flow = s:option(Flag, "sw_flow", translate("Software flow offloading"))
-sw_flow.default = 0
-sw_flow.description = translate("Software based offloading for routing/NAT")
-sw_flow:depends("sfe_flow", 0)
+    sw_flow = s:option(Flag, "sw_flow", translate("Software flow offloading"))
+    sw_flow.default = 0
+    sw_flow.description = translate("Software based offloading for routing/NAT")
+    sw_flow:depends("sfe_flow", 0)
 end
 
 if luci.sys.call("cat /proc/cpuinfo | grep -q MT76") == 0 then
-hw_flow = s:option(Flag, "hw_flow", translate("Hardware flow offloading"))
-hw_flow.default = 0
-hw_flow.description = translate("Requires hardware NAT support")
-hw_flow:depends("sw_flow", 1)
+    hw_flow = s:option(Flag, "hw_flow", translate("Hardware flow offloading"))
+    hw_flow.default = 0
+    hw_flow.description = translate("Requires hardware NAT support")
+    hw_flow:depends("sw_flow", 1)
 end
 
 if nixio.fs.access("/lib/modules/" .. kernel_version .. "/shortcut-fe-cm.ko") then
-sfe_flow = s:option(Flag, "sfe_flow", translate("Shortcut-FE flow offloading"))
-sfe_flow.default = 0
-sfe_flow.description = translate("Shortcut-FE based offloading for routing/NAT")
-sfe_flow:depends("sw_flow", 0)
+    sfe_flow = s:option(Flag, "sfe_flow", translate("Shortcut-FE flow offloading"))
+    sfe_flow.default = 0
+    sfe_flow.description = translate("Shortcut-FE based offloading for routing/NAT")
+    sfe_flow:depends("sw_flow", 0)
 end
 
 if nixio.fs.access("/lib/modules/" .. kernel_version .. "/tcp_bbr.ko") then
-bbr_cca = s:option(Flag, "bbr_cca", translate("BBR CCA"))
-bbr_cca.default = 0
-bbr_cca.description = translate("Using BBR CCA can improve TCP network performance effectively")
-end 
+    bbr_cca = s:option(Flag, "bbr_cca", translate("BBR CCA"))
+    bbr_cca.default = 0
+    bbr_cca.description = translate("Using BBR CCA can improve TCP network performance effectively")
+end
 
 if nixio.fs.access("/lib/modules/" .. kernel_version .. "/xt_FULLCONENAT.ko") then
-fullcone_nat = s:option(Flag, "fullcone_nat", translate("FullCone NAT"))
-fullcone_nat.default = 0
-fullcone_nat.description = translate("Using FullCone NAT can improve gaming performance effectively")
-end 
+    fullcone_nat = s:option(Flag, "fullcone_nat", translate("FullCone NAT"))
+    fullcone_nat.default = 0
+    fullcone_nat.description = translate("Using FullCone NAT can improve gaming performance effectively")
+end
 
 dns_caching = s:option(Flag, "dns_caching", translate("DNS Caching"))
 dns_caching.default = 0
@@ -49,10 +49,10 @@ dns_caching.description = translate("Enable DNS Caching and anti ISP DNS polluti
 dns_caching_mode = s:option(ListValue, "dns_caching_mode", translate("Resolve DNS Mode"), translate("DNS Program"))
 dns_caching_mode:value("1", translate("Using PDNSD to query and cache"))
 if nixio.fs.access("/usr/bin/dnsforwarder") then
-dns_caching_mode:value("2", translate("Using DNSForwarder to query and cache"))
+    dns_caching_mode:value("2", translate("Using DNSForwarder to query and cache"))
 end
 if nixio.fs.access("/usr/bin/dnsproxy") then
-dns_caching_mode:value("3", translate("Using DNSProxy to query and cache"))
+    dns_caching_mode:value("3", translate("Using DNSProxy to query and cache"))
 end
 dns_caching_mode.default = 1
 dns_caching_mode:depends("dns_caching", 1)
--- root/usr/share/dnscache/dnscache-while.sh
+++ root/usr/share/dnscache/dnscache-while.sh
@@ -11,26 +11,25 @@ dns_caching_mode="$(uci -q get turboacc.config.dns_caching_mode)"
 
 clean_log() {
 	logrow="$(grep -c "" "${logfile}")"
-	[ "${logrow}" -lt "500" ] || echo "${curtime} Log 数量超过限制，已清理" > "${logfile}"
+	[ "${logrow}" -lt "500" ] || echo "${curtime} Log 数量超过限制，已清理" >"${logfile}"
 }
 
-while [ "${dns_caching}" -eq "1" ];
-do
+while [ "${dns_caching}" -eq "1" ]; do
 	curtime="$(date "+%H:%M:%S")"
 
 	clean_log
 
-	if pidof dnscache > "/dev/null"; then
-		echo -e "${curtime} online!" >> "${logfile}"
+	if pidof dnscache >"/dev/null"; then
+		echo -e "${curtime} online!" >>"${logfile}"
 	else
 		if [ "${dns_caching_mode}" = "1" ]; then
-			${dnsprogram} -c "${dnsconf}" > "${logfile}" 2>&1 &
+			${dnsprogram} -c "${dnsconf}" >"${logfile}" 2>&1 &
 		elif [ "${dns_caching_mode}" = "2" ]; then
-			${dnsprogram} -f "${dnsconf}" > "${logfile}" 2>&1 &
+			${dnsprogram} -f "${dnsconf}" >"${logfile}" 2>&1 &
 		elif [ "${dns_caching_mode}" = "3" ]; then
-			${dnsprogram} -o "${logfile}" -l "127.0.0.1" -p "5333" -b "tls://9.9.9.9" -f "tls://8.8.8.8" -u "${dnsconf}" --all-servers --cache --cache-min-ttl=3600 > "${logfile}" 2>&1 &
+			${dnsprogram} -o "${logfile}" -l "127.0.0.1" -p "5333" -b "tls://9.9.9.9" -f "tls://8.8.8.8" -u "${dnsconf}" --all-servers --cache --cache-min-ttl=3600 >"${logfile}" 2>&1 &
 		fi
-		echo "${curtime} 重启服务！" >> ${logfile}
+		echo "${curtime} 重启服务！" >>${logfile}
 	fi
 
 	sleep "${sleeptime}"
